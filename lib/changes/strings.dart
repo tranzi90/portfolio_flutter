fa2e43f55f364a2f9ce1742851a3dbee9018760d
denis_levchenko <tranzi90@gmail.com>
2023-10-03 23:12:57 +0300

    add animation text

--- lib/changes/strings.dart
+++ lib/changes/strings.dart
@@ -4,7 +4,7 @@ String yourname = "I'm Denis Levchenko,";
 // String hometitle2 = "A Mobile Application\nDeveloper";
 
 String animationtxt1 = " Mobile Application Developer";
-String animationtxt2 = " UI/UX Designer";
+String animationtxt2 = " Full Stack Developer";
 String animationtxt3 = " Web Developer";
 
 //
